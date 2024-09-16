0d6fee9dd1b7ba0c2c4ade19187dda8b69809e4f
bandey <origbandey@gmail.com>
2024-09-16 21:32:41 +0300

    Add SERV_HOST config parameter

--- config.py
+++ config.py
@@ -2,4 +2,5 @@ import os
 
 class Config:
   DEBUG = os.environ.get('FLASK_DEBUG') or False
-  SERV_PORT = os.environ.get('SERV_PORT') or 8080
+  SERV_HOST = os.environ.get('TASK_SERV_HOST') or '0.0.0.0'
+  SERV_PORT = os.environ.get('TASK_SERV_PORT') or 8080
--- graph-sharing.py
+++ graph-sharing.py
@@ -1,4 +1,4 @@
 from app import app
 
 if __name__ == '__main__':
-  app.run(port=app.config['SERV_PORT'])
+  app.run(host=app.config['SERV_HOST'], port=app.config['SERV_PORT'])
