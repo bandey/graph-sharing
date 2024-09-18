f6e1be9534fccb12faf8f4a8d668587872d84a84
bandey <origbandey@gmail.com>
2024-09-18 22:15:09 +0300

    Add draft of auth mechanics

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ class Config:
   SERV_HOST = os.environ.get('TASK_SERV_HOST') or '0.0.0.0'
   SERV_PORT = os.environ.get('TASK_SERV_PORT') or 8080
   DB_CONNECT = os.environ.get('TASK_DB_CONNECT') or 'mongodb://localhost/test'
+  SECRET_KEY = os.environ.get('TASK_SECRET_KEY') or 'A_SECRET_KEY'
--- webserver/router.py
+++ webserver/router.py
@@ -1,6 +1,23 @@
 from webserver import app
-from flask import render_template
+from flask import request, session, redirect, url_for, render_template
 
 @app.route('/')
 def index():
-  return render_template('hello.html')
+  if 'visitor' in session:
+    return render_template('hello.html')
+  else:
+    return redirect(url_for('auth'))
+
+@app.route('/auth')
+def auth():
+  return render_template('login-form.html')
+
+@app.route('/auth/enter', methods=['POST'])
+def auth_enter():
+  session['visitor'] = request.form['login']
+  return redirect(url_for('index'))
+
+@app.route('/auth/exit', methods=['GET', 'POST'])
+def auth_exit():
+  session.pop('visitor', None)  
+  return redirect(url_for('auth'))
