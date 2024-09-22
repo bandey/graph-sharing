597968fc13cc2e8d39ffa34d2a1e85f97721825e
bandey <origbandey@gmail.com>
2024-09-22 19:10:21 +0300

    Rename router functions

--- webserver/router.py
+++ webserver/router.py
@@ -2,30 +2,30 @@ from webserver import app
 from flask import request, session, redirect, url_for, render_template
 
 @app.route('/')
-def index():
+def route_index():
   if 'visitor' in session:
     return render_template('hello.html')
   else:
-    return redirect(url_for('auth'))
+    return redirect(url_for('route_auth'))
 
 @app.route('/auth')
-def auth():
+def route_auth():
   return render_template('login-form.html')
 
 @app.route('/auth/enter', methods=['POST'])
-def auth_enter():
+def route_auth_enter():
   session['visitor'] = request.form['login']
-  return redirect(url_for('index'))
+  return redirect(url_for('route_index'))
 
 @app.route('/auth/exit', methods=['GET', 'POST'])
-def auth_exit():
+def route_auth_exit():
   session.pop('visitor', None)  
-  return redirect(url_for('auth'))
+  return redirect(url_for('route_auth'))
 
 @app.route('/import', methods=['GET', 'POST'])
 def route_import():
   if 'visitor' not in session:
-    return redirect(url_for('auth'))
+    return redirect(url_for('route_auth'))
   if request.method == 'GET':
     return render_template('import.html')
   if 'dataFile' not in request.files:
