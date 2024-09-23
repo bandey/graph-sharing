c903e6f660739b19d73f6ea5be29344470778dd5
bandey <origbandey@gmail.com>
2024-09-23 19:30:02 +0300

    Add processing of relates in import_graph.py action

--- actions/import_graph.py
+++ actions/import_graph.py
@@ -1,5 +1,5 @@
 import json
-from database.models import graph, vertex
+from database.models import graph, vertex, edge
 
 def importGraph(file, name):
   # go to begin of file
@@ -26,5 +26,17 @@ def importGraph(file, name):
     elem['graph_id'] = graph_id
     # save person in database
     vertex.insert(elem)
+
+  # loop all relates
+  for elem in root['relates']:
+    # rename special fields _from and _to
+    elem['source'] = elem['_from']
+    del elem['_from']
+    elem['target'] = elem['_to']
+    del elem['_to']
+    # add graph_id field
+    elem['graph_id'] = graph_id
+    # save relate in database
+    edge.insert(elem)
   
   return 'Данные из файла успешно импортированы'
