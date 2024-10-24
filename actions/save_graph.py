3d27e754a2d10d42796f7683123cfe42abc5fba2
bandey <origbandey@gmail.com>
2024-10-24 17:01:33 +0300

    Add renaming of graph to save_graph.py action

--- actions/save_graph.py
+++ actions/save_graph.py
@@ -1,14 +1,24 @@
 import json
 from database.models import graph, vertex
+from bson.objectid import ObjectId
 
 def saveGraph(graphId, jsonData):
   # parse json string
   data = json.loads(jsonData)
 
+  # check given graph name
+  graphName = data['name']
+  if len(graphName) < 1:
+    return 'Ошибка: Не указано имя графа'
+  # check if another graph with given name already exist
+  testGraph = graph.getOneByName(graphName)
+  if testGraph and (testGraph['_id'] != ObjectId(graphId)):
+    return 'Ошибка: Граф с указанным названием уже есть'
+
   # loop all vertices
-  for elem in data:
+  for elem in data['pos']:
     vertex.updateByIds(graphId, elem['id'], {'x': elem['x'], 'y': elem['y']})
 
-  graph.update(graphId, {'layout': 'preset'})
+  graph.update(graphId, {'name': graphName, 'layout': 'preset'})
 
   return 'Данные успешно сохранены'
