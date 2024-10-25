e744ba13a99b1af126650574e248df3023fdb9e6
bandey <origbandey@gmail.com>
2024-10-26 00:13:20 +0300

    Remake class of vertices and edges to array

--- actions/import_graph.py
+++ actions/import_graph.py
@@ -28,10 +28,10 @@ def importGraph(file, name):
   root = data[0]
 
   # add new graph to database
-  graph_id = graph.insert({'name': graphName})
+  graph_id = graph.insert({'name': graphName, 'kind': root.get('kind', '')})
 
   # loop all vertices
-  for elem in root['vertices']:
+  for elem in root.get('vertices', []):
     # rename special fields _id and _key
     elem['id'] = elem['_id']
     del elem['_id']
@@ -40,14 +40,14 @@ def importGraph(file, name):
     # drop special field _rev
     del elem['_rev']
     # add class field
-    elem['class'] = elem['id'].split('/',1)[0]
+    elem['class'] = [elem['id'].split('/',1)[0]]
     # add graph_id field
     elem['graph_id'] = graph_id
     # save vertex in database
     vertex.insert(elem)
 
   # loop all edges
-  for elem in root['edges']:
+  for elem in root.get('edges', []):
     # rename special fields _id, _from and _to
     elem['id'] = elem['_id']
     del elem['_id']
@@ -59,7 +59,7 @@ def importGraph(file, name):
     del elem['_key']
     del elem['_rev']
     # add class field
-    elem['class'] = elem['id'].split('/',1)[0]
+    elem['class'] = [elem['id'].split('/',1)[0]]
     # add graph_id field
     elem['graph_id'] = graph_id
     # save edge in database
