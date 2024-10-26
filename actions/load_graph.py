66854e74644025c4533cd6035b591f9c5ab09307
bandey <origbandey@gmail.com>
2024-10-26 12:16:58 +0300

    Add decoration of vertices and edges in load_graph.py action

--- actions/load_graph.py
+++ actions/load_graph.py
@@ -4,6 +4,27 @@ def loadGraph(id):
   g = graph.getOne(id)
   if not g:
     return None
-  g['vertices'] = vertex.getByGraphId(id)
-  g['edges'] = edge.getByGraphId(id)
+  kind = g.get('kind', '')
+
+  vs = vertex.getByGraphId(id)
+  es = edge.getByGraphId(id)
+
+  # Decorate vertices
+  vertices = []
+  for elem in vs:
+    elem['label'] = elem['name']
+    vertices.append(elem)
+
+  # Decorate edges
+  edges = []
+  for elem in es:
+    if kind == 'relatives':
+      elem['label'] = '%s [%s]' % (elem['name'], ','.join(elem['origins']))
+    else:
+      elem['label'] = elem['name']
+    edges.append(elem)
+
+  g['vertices'] = vertices
+  g['edges'] = edges
+
   return g
